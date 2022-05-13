e1d435c5af16fb8310dfe0f300bcd79e6186a862
Loren Meta <74499033+LorenMeta@users.noreply.github.com>
2022-05-13 08:06:25 +0200

    Update index.js

--- seeds/index.js
+++ seeds/index.js
@@ -18,16 +18,32 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 25) + 10;
         const camp = await new Campground({
             author: '625a86c1e8183ff2914065ab',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/484351',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam maxime, quo maiores assumenda optio magnam error sed ducimus harum inventore minus aliquam quod similique sint possimus? Sapiente quo vitae at!',
-            price
+            price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude
+                ]
+            },
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/lorenyelpcamp/image/upload/v1652336192/YelpCamp/juzjsdllzgc141yb1iwm.jpg',
+                    filename: 'YelpCamp/juzjsdllzgc141yb1iwm'
+                },
+                {
+                    url: 'https://res.cloudinary.com/lorenyelpcamp/image/upload/v1652336162/YelpCamp/rsh4a9deipsyopq2xsqm.jpg',
+                    filename: 'YelpCamp/rsh4a9deipsyopq2xsqm'
+                }
+            ]
         })
         await camp.save();
     }
